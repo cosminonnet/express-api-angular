26f8b3ff8225d23415bb88294f5937961589dd87
cmolea <cmolea@unitedmotionideas.com>
2014-04-07 16:29:35 +0200

    Generation of Angular project.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('expressApiAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
